d9e70e6dd95bbe5cccf487367460d076109b59a8
Wieland Hoffmann <themineo@gmail.com>
2020-05-30 20:15:52 +0200

    Remove all files I don't really need

--- get-last-wikidump.sh
+++
@@ -1,7 +0,0 @@
-LANG=$1
-FILE=${LANG}wiki-latest-all-titles-in-ns0.gz
-
-rm ${FILE}
-wget http://dumps.wikimedia.org/${LANG}wiki/latest/${FILE}
-
-gunzip -f ${FILE}
--- runtests.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-import doctest
-
-doctest.testfile('tests/split_artists.txt', optionflags=doctest.REPORT_CDIFF)
-
